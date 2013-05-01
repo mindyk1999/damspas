6419ab79acd717aabd1326a11ed9e8d7ebb2033b
Esmé Cowles <escowles@ucsd.edu>
2013-05-01 15:23:49 -0400

    Removing debug logging, updating edit form (DHH-79)

--- app/models/x_vocabulary.rb
+++ app/models/x_vocabulary.rb
@@ -1,5 +1,5 @@
 class XVocabulary < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => XVocabularyDatastream 
   delegate_to "damsMetadata", [:description,:rdf_subject]
-  has_many :x_vocabulary_entries, :property => :vocabulary
+  has_many :x_vocabulary_entries, :property => :vocabularyEntry, :inverse_of => :x_vocabulary
 end
--- app/models/x_vocabulary_entry.rb
+++ app/models/x_vocabulary_entry.rb
@@ -1,5 +1,5 @@
 class XVocabularyEntry < ActiveFedora::Base
   has_metadata 'damsMetadata', :type => XVocabularyEntryDatastream 
   delegate_to "damsMetadata", [:code, :value, :valueURI, :authority, :authorityURI,:rdf_subject]
-  belongs_to :x_vocabulary, :property => 'dams:vocabulary', :class_name => 'XVocabulary'
+  belongs_to :x_vocabulary, :property => 'dams:vocabulary', :inverse_of => :vocabulary
 end
