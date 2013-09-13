779904c8905f80a7297fd97b35b272dc354aa4f1
Esmé Cowles <escowles@ucsd.edu>
2013-09-13 12:08:10 -0400

    Updating tests and config for Rails 4.0, getting rights metadata indexed across more record types (DHH-184)

--- config/environments/gimili.rb
+++ config/environments/gimili.rb
@@ -7,7 +7,7 @@ Hydra::Application.configure do
   config.cache_classes = false
 
   # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+  config.eager_load = false
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
@@ -21,10 +21,6 @@ Hydra::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
   config.assets.compress = false
