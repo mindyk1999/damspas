7953b8e0cb78d853349c24cd9045c1a56f1ed14a
Esmé Cowles <escowles@ucsd.edu>
2013-10-10 09:40:00 -0400

    Moving developer-auth groups to environment-specific config, making collection search handle removing collection facet on initial display (DHH-167)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,4 +71,5 @@ Hydra::Application.configure do
   config.role_vocab = "#{config.id_namespace}bb14141414"
   config.lang_vocab ="#{config.id_namespace}bb43434343"
   config.excluded_collections = "(bd5905379f OR bb13664503)"
+  config.developer_groups = ['public']
 end
