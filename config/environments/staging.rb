0dcde4498b1839257a2b8c8b7a05e02740b0cfc2
lsitu <lsitu@ucsd.edu>
2013-12-04 11:12:14 -0800

    Updated the staging serve to use http://librarytest.ucsd.edu/zoomify for tiles access.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -37,6 +37,6 @@ Hydra::Application.configure do
   config.lang_vocab ="#{config.id_namespace}bb43434343"
   config.excluded_collections = "(bd5905379f OR bb13664503)"
   config.developer_groups = ['developer-authenticated','dams-curator','dams-manager-admin', 'dams-manager-user']
-  config.zoomify_baseurl = 'http://rohan.ucsd.edu/zoomify/'
+  config.zoomify_baseurl = 'http://librarytest.ucsd.edu/zoomify/'
   config.shibboleth = true
 end
